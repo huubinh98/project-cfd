a66c7815d9b2ff688bc845fb190c78a1c42e0f29
Tran Huu Binh <tranhuubinh0198@gmail.com>
2021-12-04 14:25:09 +0700

    commit js gbox

--- gbox/dest/main.js
+++ gbox/dest/main.js
@@ -1,1 +1,85 @@
-console.log('Hello CFD Circle');
+$(window).on('load', function () {
+    let load = $('.load')
+    // load.css({
+    //     "transform": "translateY(-100%)",
+    //     "opacity": "0"
+    // })
+    load.fadeOut(2000)
+})
+
+$(document).ready(function () {
+
+    // ACTIVE NAV-------------
+    $('.hambuger').on('click', function(e) {
+        e.stopPropagation();
+        $('.nav').toggleClass('active')
+        $(this).toggleClass('clicked')
+    })
+    $('.nav').on('click', function(e) {
+        e.stopPropagation();
+    })
+
+    // Active booking
+    $('.booking__package li').on('click', function() {
+        $('.booking__package li').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    // category project
+    $('.project__ctg-item').on('click', function(e) {
+        e.preventDefault()
+        $('.project__ctg-item').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    // Scroll Top-----------------
+    $('.backto').on('click', function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+
+    // Window click
+    $(window).on('click', function() {
+        $('.nav').removeClass('active')
+        $('.hambuger').removeClass('clicked')
+    })
+
+    // Window resize
+    $(window).on('resize', function() {
+        let windowWidth = $(window).outerWidth();
+        if(windowWidth > 992) {
+            $('.nav').removeClass('active')
+            $('.hambuger').removeClass('clicked')
+        }
+    })
+})
+
+$('.swiper__list').flickity({
+    cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,
+    lazyLoad: 2,
+    fullscreen: true,
+});
+
+$('.gallery__list').flickity({
+    cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,
+    pageDots: false,
+    lazyLoad: 2,
+})
+
+$(".fullscreen").on('click', function() {
+    $('.swiper__list').flickity('viewFullscreen')
+})
+
+$(".prev").on('click', function() {
+    $('.swiper__list').flickity('previous')
+    $('.gallery__list').flickity('previous')
+})
+
+$(".next").on('click', function() {
+    $('.swiper__list').flickity('next')
+    $('.gallery__list').flickity('next')
+})
